a108bde7c625122b96bc418e0779b5c93af61236
abalabin <abalabin@student.ucode.world>
2020-08-29 11:56:52 +0300

    first push

---
+++ app/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
+
+using namespace curlpp::options;
+
+int main() {
+
+    try
+    {
+        // That's all that is needed to do cleanup of used resources (RAII style).
+        curlpp::Cleanup myCleanup;
+
+        // Our request to be sent.
+        curlpp::Easy myRequest;
+
+        // Set the URL.
+        myRequest.setOpt<Url>("https://github.com/");
+
+        // Send request and get a result.
+        // By default the result goes to standard output.
+        myRequest.perform();
+    }
+
+    catch(curlpp::RuntimeError & e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    catch(curlpp::LogicError & e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+}
---
+++ app/src/ThreadPool.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Anton Balabin on 8/29/20.
+//
+
+#include "ThreadPool.h"
