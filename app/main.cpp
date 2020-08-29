6ef086dbd0f75729356c2413454f5746cae66c72
Zend0r-ai <vlad98kn@gmail.com>
2020-08-29 19:04:24 +0300

    lyaa

--- app/main.cpp
+++ app/main.cpp
@@ -17,57 +17,42 @@ std::string strval = "A string";
 test_enum enumval = Item2;
 Color colval(0.5f, 0.5f, 0.7f, 1.f);
 
+
 int main(int /* argc */, char ** /* argv */) {
     nanogui::init();
 
-    /* scoped variables */ {
-        bool use_gl_4_1 = false;// Set to true to create an OpenGL 4.1 context.
-        Screen *screen = nullptr;
+    Screen *screen = nullptr;
+    screen = new Screen(Vector2i(600, 500), "RACE01-C-Parsing", false);
+    Widget *panel = new Widget(screen);
+    panel->setLayout(new BoxLayout(Orientation::Vertical, Alignment::Middle, 10, 5));
+    panel->setPosition({90, 50});
+    TextBox *text = new TextBox(panel, "");
+    text->setFixedWidth(400);
+    text->value();
+    text->setEditable(true);
 
-        if (use_gl_4_1) {
-            // NanoGUI presents many options for you to utilize at your discretion.
-            // See include/nanogui/screen.h for what all of these represent.
-            screen = new Screen(Vector2i(500, 700), "NanoGUI test [GL 4.1]",
-                /*resizable*/true, /*fullscreen*/false, /*colorBits*/8,
-                /*alphaBits*/8, /*depthBits*/24, /*stencilBits*/8,
-                /*nSamples*/0, /*glMajor*/4, /*glMinor*/1);
-        } else {
-            screen = new Screen(Vector2i(500, 700), "NanoGUI test");
-        }
+    Button *parse_button = new Button(panel, "Parse");
+    parse_button->setFixedWidth(100);
+    parse_button->setCallback([text](){
+       std::cout << text->value() << std::endl;
+        text->setValue("");
+    });
 
-        bool enabled = true;
-        FormHelper *gui = new FormHelper(screen);
-        ref<Window> window = gui->addWindow(Eigen::Vector2i(10, 10), "Form helper example");
-        gui->addGroup("Basic types");
-        gui->addVariable("bool", bvar);
-        gui->addVariable("string", strval);
+    Button *pause_button = new Button(panel, "Pause");
+    pause_button->setFixedWidth(100);
+    pause_button->setCallback([text](){
+        std::cout << "pause" << std::endl;
+    });
 
-        gui->addGroup("Validating fields");
-        gui->addVariable("int", ivar)->setSpinnable(true);
-        gui->addVariable("float", fvar);
-        gui->addVariable("double", dvar)->setSpinnable(true);
+//    MessageDialog *mess = new MessageDialog(screen, nanogui::MessageDialog::Type::Information, "");
 
-        gui->addGroup("Complex types");
-        gui->addVariable("Enumeration", enumval, enabled)
-            ->setItems({"Item 1", "Item 2", "Item 3"});
-        gui->addVariable("Color", colval)
-            ->setFinalCallback([](const Color &c) {
-                std::cout << "ColorPicker Final Callback: ["
-                          << c.r() << ", "
-                          << c.g() << ", "
-                          << c.b() << ", "
-                          << c.w() << "]" << std::endl;
-            });
-
-        gui->addGroup("Other widgets");
-        gui->addButton("A button", []() { std::cout << "Button pressed." << std::endl; });
-
-        screen->setVisible(true);
-        screen->performLayout();
-        window->center();
-
-        nanogui::mainloop();
-    }
+    TextBox *res_text = new TextBox(panel, "geajgeoshgpoesuhgpoeuhgrpoeashrgoubeoiu rgbesoiubfrgseoibdrgoaeurghpoaeuihrgoaeugrpeioubge gpeorhgpeoru hgpeoirhgoeuhrg ouehpog herapoirghepoaihr[");
+    res_text->setFixedWidth(400);
+    res_text->setFixedSize({400, 400});
+//    VScrollPanel scroll = new VScrollPanel(res_text);
+    screen->setVisible(true);
+    screen->performLayout();
+    nanogui::mainloop();
 
     nanogui::shutdown();
     return 0;
